751f9bffd415768a934303cb33ce38257a8a5f0a
Helias <stefanoborzi32@gmail.com>
2017-06-06 10:08:20 +0200

    Added config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'secret': 'secretstring',
+    'database': 'mongodb://cytoscape:sK0tMWUfzNkfOzx9FNrccGjhtkwPU0l0tH2GeC9stMEWKmKjfQzwThuXKGYozheLsMhbw3pnJocZrKMuAcgFpA==@cytoscape.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false',
+    'email': 'azurecytoscape@gmail.com',
+    'password': 'mCG-2v8-Lub-rFm'
+};
