68a9b8023a74dbadb96edae9fef9f783aa2ebb91
Helias <stefanoborzi32@gmail.com>
2017-06-07 18:58:12 +0200

    Added contact page

--- public/js/routes.js
+++ public/js/routes.js
@@ -5,41 +5,39 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
 
   $stateProvider
-
-    .state("Login", {
-    url: "/Login",
-    templateUrl: "partials/login.html",
-    controller:"LoginController"
+    .state("login", {
+      url: "/login",
+      templateUrl: "partials/login.html",
+      controller:"LoginController"
     })
-
-    .state("Register", {
-    url: "/Register",
-    templateUrl: "partials/register.html",
-    controller: "RegisterController"
+    .state("register", {
+      url: "/register",
+      templateUrl: "partials/register.html",
+      controller: "RegisterController"
     })
-
     .state("profile", {
-    url: '/profile',
-    templateUrl: "partials/profile.html",
-    controller:"profileController"
+      url: '/profile',
+      templateUrl: "partials/profile.html",
+      controller:"profileController"
     })
-
     .state("seller", {
-    url: '/seller',
-    templateUrl: "partials/seller.html",
-    controller:"sellerController"
+      url: '/seller',
+      templateUrl: "partials/seller.html",
+      controller:"sellerController"
     })
-
     .state('initial', {
       url: '/home',
       templateUrl: 'partials/home.html',
       controller: 'homeController'
     })
-
     .state("showcase", {
-	  url: "/showcase",
-	  templateUrl: "partials/showcase.html",
-	  controller:"showCaseController"
-	});
+  	  url: "/showcase",
+  	  templateUrl: "partials/showcase.html",
+  	  controller:"showCaseController"
+    })
+    .state("contact", {
+  	  url: "/contact",
+  	  templateUrl: "partials/contact.html"
+    });
 
 });
